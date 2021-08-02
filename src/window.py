92f700061443848507484bc85ea2250970255efd
kphan20 <knpmths@gmail.com>
2021-08-01 20:18:49 -0400

    Readjusted tile coloring algorithm and added class variables to Character class

--- src/window.py
+++ src/window.py
@@ -1,10 +1,10 @@
 import pyglet
-import pyglet
 from pyglet.window import key
 from game import resources
-from utils import *
+import utils
 #Make config object for window
-#pyglet.options['search_local_libs'] = True
+print(resources.__dict__)
+# pyglet.options['search_local_libs'] = True
 source = resources.source
 source.play()
 class StartingScreen(pyglet.sprite.Sprite):
@@ -16,20 +16,23 @@ class StartingMenu(pyglet.sprite.Sprite):
 class Character(pyglet.sprite.Sprite):
     def __init__(self, img, batch, group):
         super().__init__(img=img, batch=batch, group=group)
-class Tile(pyglet.sprite.Sprite):
-    def __init__(self, img, x, y, batch, group):
-        super().__init__(img=img, x=x, y=y, batch=batch, group=group)
-        self.scale= TILE_SCALE
-        self.character = None
-        self.arrow = None
-    def change_tint(self, color):
-        self.color = color
-    def set_character(self, character):
-        self.character = character
-        self.character.position = (self.x, self.y)
-    def draw(self):
-        super().draw()
-        self.character.draw()
+        
+        
+        # Used for game classes (eg. paladin, assassin, etc.)
+        self.class_type = None
+        # Can be pulled from class_type
+        self.valid_weapons = {}
+        
+        # Individual based stats
+        #self.battle_sprite = pyglet.sprite.Sprite() # used for battle animation
+        self.stats = {}
+        self.weapons = {}
+        self.growths = {}
+        self.team = None # Probably use ints for team numbers
+
+class Class():
+    pass
+
 class TileScreen():
     def __init__(self, batch):
         batch.draw()
@@ -43,71 +46,78 @@ class Window(pyglet.window.Window):
         self.batch = pyglet.graphics.Batch()
         self.background = pyglet.graphics.OrderedGroup(0)
         self.foreground = pyglet.graphics.OrderedGroup(1)
-        offset_x = 100
-        offset_y = 100
-        self.tiles=[]
-        for x in range(5):
-            row = []
-            for y in range(5):
-                row.append(Tile(resources.tile, offset_x, offset_y, self.batch, group=self.background))
-                offset_x += TILE_SIZE * TILE_SCALE
-            offset_x = 100
-            offset_y += TILE_SIZE * TILE_SCALE
-            self.tiles.append(row)
+        self.tiles=utils.generate_map_tiles(self.batch, self.background)
+    
         self.current_x= 0
         self.current_y = 0
         #self.characters = []
         self.test_character = Character(resources.player_image, self.batch, self.foreground)
         self.tiles[self.current_x][self.current_y].set_character(self.test_character)
-
+    def reset_tiles(self):
+        for y in range(len(self.tiles)):
+            for x in range(len(self.tiles[0])):
+                self.tiles[y][x].change_tint(utils.NORMAL_TINT)
     def move_finder(self, current_x, current_y, max_move, attack_range):
         #assuming rectangular map
         width = len(self.tiles[0])
         height = len(self.tiles)
         filler = [[0 for x in range(width)] for x in range(height)]
-        self.fill_move(current_x, current_y, max_move, filler)
-        #self.fill_attacks(current_x, current_y, attack_range, filler)
+        def fill_move(current_x, current_y, max_move, filler):
+            if current_x < 0 or current_x >= len(filler[0]) or current_y < 0 or current_y >= len(filler):
+                return
+            if filler[current_y][current_x] < max_move:
+                filler[current_y][current_x] = max_move
+            if max_move == 1:
+                fill_attacks(current_x, current_y, attack_range + 1, filler)
+                return
+            fill_move(current_x + 1, current_y, max_move - 1, filler)
+            fill_move(current_x - 1, current_y, max_move - 1, filler)
+            fill_move(current_x, current_y + 1, max_move - 1, filler)
+            fill_move(current_x, current_y - 1, max_move - 1, filler)
+        def fill_attacks(current_x, current_y, attack_range, filler):
+            if attack_range <= 0 or current_x < 0 or current_x >= len(filler[0]) or current_y < 0 or current_y >= len(filler):
+                return
+            if filler[current_y][current_x] <= 0 and filler[current_y][current_x] > -attack_range:
+                filler[current_y][current_x] = -attack_range
+            fill_attacks(current_x + 1, current_y, attack_range - 1, filler)
+            fill_attacks(current_x - 1, current_y, attack_range - 1, filler)
+            fill_attacks(current_x, current_y + 1, attack_range - 1, filler)
+            fill_attacks(current_x, current_y - 1, attack_range - 1, filler)
+        fill_move(current_x, current_y, max_move + 1, filler)
         return filler
-    def reset_tiles(self):
-        for y in range(len(self.tiles)):
-            for x in range(len(self.tiles[0])):
-                self.tiles[y][x].change_tint(NORMAL_TINT)
+
     def color_tiles(self, filler):
         width = len(filler[0])
         for y in range(len(filler)):
             for x in range(width):
                 if filler[y][x] > 0:
-                    self.tiles[y][x].change_tint(BLUE_TINT)
+                    self.tiles[y][x].change_tint(utils.BLUE_TINT)
                 elif filler[y][x] < 0:
-                    self.tiles[y][x].change_tint(RED_TINT)
-    def fill_move(self, current_x, current_y, maxMove, filler):
-        if current_x < 0 or current_x > len(filler[0]) or current_y < 0 or current_y > len(filler):
-            return
-        try:
-            if filler[current_y][current_x] < maxMove:
-                filler[current_y][current_x] = maxMove
-        except:
-            print(current_x)
-            print(current_y)
-        if maxMove == 1:
-            return
-        self.fill_move(current_x + 1, current_y, maxMove - 1, filler)
-        self.fill_move(current_x - 1, current_y, maxMove - 1, filler)
-        self.fill_move(current_x, current_y + 1, maxMove - 1, filler)
-        self.fill_move(current_x, current_y - 1, maxMove - 1, filler)
+                    self.tiles[y][x].change_tint(utils.RED_TINT)
+    # def fill_move(self, current_x, current_y, max_move, filler):
+    #     if current_x < 0 or current_x > len(filler[0]) or current_y < 0 or current_y > len(filler):
+    #         return
+    #     try:
+    #         if filler[current_y][current_x] < max_move:
+    #             filler[current_y][current_x] = max_move
+    #     except:
+    #         print(current_x)
+    #         print(current_y)
+    #     if max_move == 1:
+    #         self.fill_attacks(current_x, current_y, attack_range, filler)
+    #         return
+    #     self.fill_move(current_x + 1, current_y, max_move - 1, filler)
+    #     self.fill_move(current_x - 1, current_y, max_move - 1, filler)
+    #     self.fill_move(current_x, current_y + 1, max_move - 1, filler)
+    #     self.fill_move(current_x, current_y - 1, max_move - 1, filler)
     def fill_attacks(self, current_x, current_y, attack_range, filler):
         if current_x < 0 or current_x > len(filler[0]) or current_y < 0 or current_y > len(filler):
             return
-        if filler[current_y][current_x] == 1:
-            self.attack_helper(current_x + 1, current_y, attack_range, filler)
-            self.attack_helper(current_x - 1, current_y, attack_range, filler)
-            self.attack_helper(current_x, current_y + 1, attack_range, filler)
-            self.attack_helper(current_x, current_y - 1, attack_range, filler)
-        else:
-            self.fill_attacks(current_x + 1, current_y, attack_range, filler)
-            self.fill_attacks(current_x - 1, current_y, attack_range, filler)
-            self.fill_attacks(current_x, current_y + 1, attack_range, filler)
-            self.fill_attacks(current_x, current_y - 1, attack_range, filler)
+        self.attack_helper(current_x + 1, current_y, attack_range, filler)
+        self.attack_helper(current_x - 1, current_y, attack_range, filler)
+        self.attack_helper(current_x, current_y + 1, attack_range, filler)
+        self.attack_helper(current_x, current_y - 1, attack_range, filler)
+
     def attack_helper(self, current_x, current_y, attack_range, filler):
         if attack_range == 0 or current_x < 0 or current_x > len(filler[0]) or current_y < 0 or current_y > len(filler):
             return
@@ -158,8 +168,8 @@ class Window(pyglet.window.Window):
                 self.character_move()
     def character_move(self):
         self.tiles[self.current_y][self.current_x].set_character(self.test_character)
-        self.tiles[self.current_y][self.current_x].change_tint(BLUE_TINT)
-        self.color_tiles(self.move_finder(self.current_x, self.current_y, 3, 1))
+        self.tiles[self.current_y][self.current_x].change_tint(utils.BLUE_TINT)
+        self.color_tiles(self.move_finder(self.current_x, self.current_y, 2, 2))
     def bounds(self, list, index):
         if index == len(list):
             return 0
